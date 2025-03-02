50a14e1d1d23f679174ecccbc6c932d3d1a31bc5
asma-abichou <abichou.asmaa@gmail.com>
2025-03-02 23:10:12 +0100

    Event

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,25 @@
+import {Component} from "react";
+
+export default class Counter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0,
+                       date : undefined,
+                     };
+    }
+    componentDidMount() {
+        setInterval(()=>
+            this.setState(
+                prevState => {
+                    return{ counter : prevState.counter + 1 }
+                },
+            )
+        ,1000);
+    }
+
+    render(){
+        return <div> Il y'a {this.state.counter }  Seconds </div>
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Counter from './counter/Counter';
 import Fruit from "./fruits/fruit";
 import HelloWorld from "./helloWorld/HelloWorld";
 import TextField from "./forms/TextField";
+import Event from "./Events/Event";
 //import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const fruitsArray = ['Strawberry' , 'Lemon', 'Mango', 'Apple', 'Water Melon' ]
-root.render(
-    <React.StrictMode>
-        {/*<HelloWorld lastName='Assouma'/>
+
+        /*<HelloWorld lastName='Assouma'/>
         <TextField inputName='firstName' label='First Name'/>
         <TextField inputName='lastName' label='Last Name'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </TextField>*/}
-        {
+        </TextField>*/
+       /* {
             <Fruit fruits={fruitsArray}/>
-        }
-    </React.StrictMode>
-);
+        }*/
+
+root.render(<Event />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
